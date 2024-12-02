539f62e1bd5850d6239c12c2a611df200d7e856b
DipanjanPanja007 <dipanjanpanja1411@gmail.com>
2024-12-03 00:52:30 +0530

    frontend: tailwind and shadcn set up complete

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,14 +1,13 @@
 import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
 import './App.css'
+import { Button } from './components/ui/button'
 
 function App() {
-  const [count, setCount] = useState(0)
 
   return (
     <>
-      ChatSphere
+      <h1 className='bg-sky-700 font-bold underline'>ChatSphere</h1>
+      <Button className='bg-amber-400'>Hello Jii</Button>
     </>
   )
 }
